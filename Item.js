3502ce6b72cd008a4005a5b25303cc50c62d461d
James Li <james.li@opower.com>
2018-07-23 11:48:21 -0700

    Inital commit

---
+++ Item.js
@@ -0,0 +1,45 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var ItemSchema = new Schema({
+    Brand: String,
+    StyleNumber: String,
+    ProductName: String,
+    SubBrand: String,
+    Size: String,
+    ColorCode: String,
+    ColorName: String,
+    ColorFamily: String,
+    UPC: {type: String, unique: true},
+    MSRP: Number,
+    WHSP: Number,
+    Price: Number,
+    Description: String,
+    CategoryName: String,
+    SubCategory: String,
+    UnitPerPack: Number,
+    PackageWeight: Number,
+    FabricContent: String,
+    CountryOfOrigin: String,
+    Quantity: Number,
+    InventoryStatus: String,
+    BackorderStatus: String,
+    BackorderDate: Date,
+    ImageURL1: String,
+    ImageURL2: String,
+    ImageURL3: String,
+    ImageURL4: String,
+});
+
+exports.create = function(req, res) {
+    ItemSchema.create(req.body, function(err, result){
+       if (!err) {
+           return res.send(result);
+       }
+       else {
+           return res.send(err);
+       }
+    });
+};
+// Item is the singular name of the collection (items) our model is for
+module.exports = mongoose.model('Item', ItemSchema);
